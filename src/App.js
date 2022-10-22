45ef891dbd5ce5aadefcdf4d34a12ea725063916
Riday1 <rrriday100@gmail.com>
2022-10-22 12:27:33 +0600

    Comments component is delared inside App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Comments from './Components/Comments/Comments';
 
 function App() {
   return (
     <div className="App">
-
+      <Comments></Comments>
     </div>
   );
 }
